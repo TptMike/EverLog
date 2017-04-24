3be9b5f02e1788a751e73679db6d9d86dd834760
Mike <mike.wildcats@gmail.com>
2017-04-24 17:10:07 -0400

    Plugin Setup
    Theres really nothing to this commit other than it happened

---
+++ src/com/everneth/everlog/EverLog.java
@@ -0,0 +1,17 @@
+package com.everneth.everlog;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EverLog extends JavaPlugin 
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
