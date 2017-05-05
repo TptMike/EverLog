59d829b09b06ed5574c8ee3edd386062e3ee9780
Mike <mike.wildcats@gmail.com>
2017-05-04 22:44:36 -0400

    Test Commit to Dev branch
    Work should be done from here now

---
+++ src/com/everneth/actions/BaseAction.java
@@ -0,0 +1,19 @@
+package com.everneth.actions;
+
+// Java libs
+import java.util.Date;
+import java.text.DateFormat;
+
+// EverLog libs
+import com.everneth.everlog.EverLog;
+// TODO: EL DB Libs
+
+// Bukkit libs
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class BaseAction {
+
+}
